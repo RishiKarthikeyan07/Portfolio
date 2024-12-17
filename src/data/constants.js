0380ad6537f534a0634282b5e28984471034515d
Ri2506 <ai.scientist01@gmail.com>
2024-12-17 22:17:44 +0000

    files commited

---
+++ src/data/constants.js
@@ -0,0 +1,261 @@
+export const Bio = {
+  name: "Rishi Karthikeyan",
+  roles: [
+    "AI Engineer",
+    "Data Scientist",
+    "Python Developer",
+    "Programmer",
+  ],
+  description:
+    "As a dedicated AI Engineer, I specialize in designing and implementing advanced machine learning, deep learning, and AI-driven solutions that solve complex challenges and drive business transformation. With a strong foundation in data analysis, natural language processing, and computer vision, I create scalable and high-performance systems that optimize processes and deliver actionable insights. I leverage cutting-edge technologies and continuously stay ahead of industry trends to ensure innovative solutions that meet both technical and business objectives. My ability to collaborate effectively with cross-functional teams ensures seamless integration and impactful results.",
+  github: "https://github.com/Ri2506",
+  resume:
+    "https://1drv.ms/b/s!AlG1XHM-Lpqib-EUycczwy-k0dQ?e=IrLYkn",
+  linkedin: "https://www.linkedin.com/in/rishi-karthikeyan-504bb5269",
+  twitter: "https://twitter.com/",
+  insta: "https://www.instagram.com/",
+  facebook: "https://www.facebook.com/",
+};
+
+export const skills = [
+  {
+    title: "Machine Learning",
+    skills: [
+      {
+        name: "Python",
+        image:
+          "https://raw.githubusercontent.com/devicons/devicon/master/icons/python/python-original.svg",
+      },
+      {
+        name: "Tenserflow",
+        image:
+          "https://static-00.iconduck.com/assets.00/tensorflow-icon-1911x2048-1m2s54vn.png",
+      },
+      {
+        name: "Keras",
+        image:
+          "https://miro.medium.com/v2/resize:fit:600/1*DKu_54iqz6C-p6ndo7rO3g.png",
+      },
+      {
+        name: "Jupyter",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/3/38/Jupyter_logo.svg/1767px-Jupyter_logo.svg.png",
+      },
+      {
+        name: "Google Colab",
+        image:
+          "https://imgs.search.brave.com/nPGs1rlO1-L-E34McRwXekavbiELyoGFOIDWI_ZrDMA/rs:fit:32:32:1:0/g:ce/aHR0cDovL2Zhdmlj/b25zLnNlYXJjaC5i/cmF2ZS5jb20vaWNv/bnMvNDc0ZWIzMGVj/ZWQwMDkyMWEyMDZh/NWU3OWY0OGE1NTNl/ODQ5Mzk5YjA0NTc3/ZDE4MWZkNTYxNDBm/Zjk3Njk3ZS9jb2xh/Yi5nb29nbGUv",
+      },
+      {
+        name: "Sci-kit Learn Kit",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/0/05/Scikit_learn_logo_small.svg/2560px-Scikit_learn_logo_small.svg.png",
+      },
+    ],
+  }, 
+  {
+    title: "BigData",
+    skills: [
+      
+      
+      {
+        name: "Graph Ql",
+        image: "https://www.vectorlogo.zone/logos/graphql/graphql-icon.svg",
+      },
+      {
+        name: "Python",
+        image:
+          "https://raw.githubusercontent.com/devicons/devicon/master/icons/python/python-original.svg",
+      },
+      {
+        name: "Flask",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/3/3c/Flask_logo.svg/1280px-Flask_logo.svg.png",
+      },
+      {
+        name: "Django",
+        image:
+          "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSlFFyY16N5NRhzoG24RqB7x8Ok2t8Bdgs-tuMsOBXS2Q&s",
+      },
+      {
+        name: "MySQL",
+        image:
+          "https://raw.githubusercontent.com/devicons/devicon/master/icons/mysql/mysql-original-wordmark.svg",
+      },
+      {
+        name: "Postgresql",
+        image: "https://www.postgresql.org/media/img/about/press/elephant.png",
+      },
+      {
+        name: "MongoDB",
+        image:
+          "https://raw.githubusercontent.com/devicons/devicon/master/icons/mongodb/mongodb-original-wordmark.svg",
+      },
+      
+    ],
+  },
+  {
+    title: "DevOps",
+    skills: [
+      {
+        name: "AWS",
+        image:
+          "https://download.logo.wine/logo/Amazon_Web_Services/Amazon_Web_Services-Logo.wine.png",
+      },
+      {
+        name: "Google Cloud",
+        image:
+          "https://static-00.iconduck.com/assets.00/google-cloud-platform-logo-icon-2048x1824-pg4wzspq.png",
+      },
+      {
+        name: "Docker",
+        image:
+          "https://raw.githubusercontent.com/devicons/devicon/master/icons/docker/docker-original-wordmark.svg",
+      },
+      
+     
+      
+    ],
+  },
+  
+  
+  {
+    title: "Others",
+    skills: [
+      {
+        name: "Git",
+        image:
+          "https://e7.pngegg.com/pngimages/713/558/png-clipart-computer-icons-pro-git-github-logo-text-logo-thumbnail.png",
+      },
+      {
+        name: "GitHub",
+        image:
+          "https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png",
+      },
+      
+      {
+        name: "VS Code",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/9/9a/Visual_Studio_Code_1.35_icon.svg/512px-Visual_Studio_Code_1.35_icon.svg.png?20210804221519",
+      },
+      
+      
+      {
+        name: "Figma",
+        image:
+          "https://s3-alpha.figma.com/hub/file/1481185752/fa4cd070-6a79-4e1b-b079-8b9b76408595-cover.png",
+      },
+    ],
+  },
+];
+
+export const experiences = [
+  {
+    id: 0,
+    img: "https://img.icons8.com/?size=48&id=12109&format=png",
+    role: "Data Science Intern",
+    company: "Alpha Tech Solutions, Coimbatore, India",
+    date: " 2021 -  2022",
+    desc: "Designed and implemented machine learning models to predict customer churn, achieving a 92% accuracy rate.Conducted data preprocessing, feature engineering, and exploratory data analysis on large datasets.Automated ETL pipelines using Python, reducing data preparation time by 40%.",
+    skills: [
+      "Machine Learning Algorithms",
+      "Tableau",
+      "Power BI",
+      "Spark",
+      "Hadoop",
+      "Data Preprocessing ETL",
+    ],
+    doc: "",
+  },
+  {
+    id: 1,
+    img: "https://img.icons8.com/?size=48&id=CHBf5jmRzl9y&format=png",
+    role: "Freelance AI/ML Projects",
+    company: "AI/ML Consultant",
+    date: " 2023 - Present",
+    desc: " Delivered end-to-end AI solutions for clients in e-commerce and healthcare sectors.Developed a chatbot system using NLP libraries (SpaCy, Hugging Face Transformers).Provided cloud integration services, deploying machine learning models on AWS SageMaker.Created interactive dashboards using Tableau and Power BI, reducing client reporting time by 30%.",
+    skills: [
+      "Docker",
+      "SpaCy",
+      "AWS",
+      "Hugging Face Transformers",
+      "RNN",
+      "CNN",
+      "Transformers",
+      " Natural Language Processing",
+      
+    ],
+    doc: "",
+  },
+  
+  
+  {
+    id: 5,
+    img: "https://firebasestorage.googleapis.com/v0/b/flexi-coding.appspot.com/o/girlScript.jpeg?alt=media&token=e656a621-cf3c-4230-bf0f-e74b4cec6035",
+    role: "Open Source Contributor ",
+    company: "",
+    date: " 2023 - Present",
+    desc: "Contributed to different open-source projects and learn from industry experts",
+  },
+];
+
+export const education = [
+  {
+    id: 0,
+    img: "https://cdn.brandfetch.io/idxVE83w50/w/400/h/400/theme/dark/icon.jpeg?c=1bfwsmEH20zzEfSNTed",
+    school: "Heriot-Watt University, Edinburgh, United Kingdom",
+    date: "2023 -  2024",
+    grade: "8.41 CGPA",
+    desc: "Relevant Coursework: Deep Learning, Natural Language Processing, Reinforcement Learning, Computer Vi- sion, Advanced Machine Learning.Thesis: Automated Stock Trading Bot Using Transformers With Optimized PSO: Designed a Transformer- based architecture to predict stock prices and optimize trading strategies using Particle Swarm Optimization, achieving a 12% improvement in portfolio returns.",
+    degree: "MSc in Artificial Intelligence",
+  },
+  {
+    id: 1,
+    img: "https://cdn.brandfetch.io/idRWsq2zPV/w/48/h/48/theme/dark/logo.png?c=1bfwsmEH20zzEfSNTed",
+    school: "Sri Krishna Arts and Science college, Coimbatore, India",
+    date: " 2021 -  2022",
+    grade: "88.2%",
+    desc: "Relevant Coursework: Data Structures, Algorithms, Database Systems, Programming Paradigms",
+    degree: "BSc in Computer Science and Applications",
+  },
+  
+];
+
+export const projects = [
+  
+ 
+ 
+  {
+    id: 2,
+    title: " Sentiment Analysis:",
+    date: "Jan 2023 - 2023",
+    description:
+      "Built an NLP-based classification model using Scikit-learn, achieving 90% accuracy on customer reviews.",
+    image: "",
+    tags: ["Python", "Keras", "TensorFlow"],
+    category: "machine learning",
+    github: "",
+    webapp: "",
+    member: [
+      ,
+     
+    ],
+  },
+  
+ 
+
+  {
+    id: 8,
+    title: "Face Recognition",
+    date: " 2021",
+    description:
+      "A Face recognition python app made with OpenCV. It uses face_recognition library to detect faces. It uses the webcam to detect faces. It also has a search bar to search for a particular face.",
+    image:
+      "https://dontrepeatyourself.org/media/face-recognition-with-python-dlib-and-deep-learning_cezKZBj.png",
+    tags: ["Python", "Keras", "TensorFlow", "VGG16", "Pickle", "React"],
+    category: "machine learning",
+    github: "https://github.com/rishavchanda/Face-Recodnition-AI-with-Python",
+    webapp: "https://github.com/rishavchanda/Face-Recodnition-AI-with-Python",
+  },
+];
+
